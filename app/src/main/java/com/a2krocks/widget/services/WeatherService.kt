4474da8ae64e208f7d471ca7ab6502b94ed9cbc5
a2krocks <aakapurv@gmail.com>
2022-03-03 17:40:21 +0530

    Added getWeather() to pass location

--- app/src/main/java/com/a2krocks/widget/services/WeatherService.kt
+++ app/src/main/java/com/a2krocks/widget/services/WeatherService.kt
@@ -2,7 +2,6 @@ package com.a2krocks.widget.services
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import com.a2krocks.widget.R
 import com.a2krocks.widget.services.data.WeatherData
 import retrofit2.Call
@@ -14,24 +13,29 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.*
 
-class WeatherService(context: Context) {
-
+class WeatherService(val context: Context) {
 
 
     private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    private val retrofitBuilder = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .build()
-        .create(WeatherInterface::class.java)
-    private val retrofitData = retrofitBuilder.getData("London", context.getString(R.string.apiId)  )
 
 
-    fun getWeather(callback: (temp: Double?, desc: String?) -> Unit) {
+    fun getWeather(location: String, callback: (temp: Double?, desc: String?) -> Unit) {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(WeatherInterface::class.java)
+        val retrofitData = retrofitBuilder.getData(
+            location, context.getString(R.string.apiId)
+        )
         retrofitData.enqueue(object : Callback<WeatherData?> {
             override fun onResponse(call: Call<WeatherData?>, response: Response<WeatherData?>) {
-                val responseBody = response.body()
-                callback(responseBody?.main?.temp, responseBody?.weather?.get(0)?.description)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    callback(responseBody?.main?.temp, responseBody?.weather?.get(0)?.description)
+                } else {
+                    callback(null, null)
+                }
             }
 
             override fun onFailure(call: Call<WeatherData?>, t: Throwable) {
@@ -52,5 +56,7 @@ interface WeatherInterface {
 
 @SuppressLint("DefaultLocale")
 fun String.capitalizeWords(): String =
-    split(" ").joinToString(" ") { it.lowercase(Locale.getDefault())
-        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } }
+    split(" ").joinToString(" ") {
+        it.lowercase(Locale.getDefault())
+            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    }
