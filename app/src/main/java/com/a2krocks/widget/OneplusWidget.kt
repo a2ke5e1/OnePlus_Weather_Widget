83f9da65b30f6a9c2cafc09a37aafdd8271b0664
a2krocks <aakapurv@gmail.com>
2022-03-13 23:53:49 +0530

    Improved Weather Service

--- app/src/main/java/com/a2krocks/widget/OneplusWidget.kt
+++ app/src/main/java/com/a2krocks/widget/OneplusWidget.kt
@@ -4,21 +4,10 @@ import android.annotation.SuppressLint
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.graphics.Color
 import android.location.LocationManager
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.widget.RemoteViews
 import com.a2krocks.widget.services.WeatherService
-import com.a2krocks.widget.services.capitalizeWords
-import com.a2krocks.widget.services.data.WeatherData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import kotlin.math.roundToInt
 
 /**
  * Implementation of App Widget functionality.
@@ -56,51 +45,13 @@ class OneplusWidget : AppWidgetProvider() {
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER, 2000, 10f
         ) {
-            WeatherService(context).getWeather(it.latitude.toString(), it.longitude.toString()).enqueue(object : Callback<WeatherData?> {
-                override fun onResponse(call: Call<WeatherData?>, response: Response<WeatherData?>) {
-                    if (response.isSuccessful) {
-
-                        val temp = response.body()?.main?.temp
-                        val desc = response.body()?.weather?.get(0)?.description
-
-                        val tempText = "${temp?.roundToInt()}°"
-                        val descText = desc.toString().capitalizeWords()
-
-
-                        val spannable = SpannableString(tempText)
-                        spannable.setSpan(
-                            ForegroundColorSpan(Color.WHITE),
-                            1,
-                            tempText.length,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-
-                        // Construct the RemoteViews object
-                        views.setTextViewText(R.id.tempTextView, spannable)
-                        views.setTextViewText(R.id.descTextView, descText)
-
-                        // Instruct the widget manager to update the widget
-                        appWidgetManager.updateAppWidget(appWidgetId, views)
-                    } else {
-                        // Construct the RemoteViews object
-                        views.setTextViewText(R.id.tempTextView, "Failed to load.")
-                        views.setTextViewText(R.id.descTextView, "Failed to load.")
-
-                        // Instruct the widget manager to update the widget
-                        appWidgetManager.updateAppWidget(appWidgetId, views)
-                    }
-                }
-
-                override fun onFailure(call: Call<WeatherData?>, t: Throwable) {
-                    // Construct the RemoteViews object
-                    views.setTextViewText(R.id.tempTextView, "Failed to load.")
-                    views.setTextViewText(R.id.descTextView, "Failed to load.")
-
-                    // Instruct the widget manager to update the widget
-                    appWidgetManager.updateAppWidget(appWidgetId, views)
-                }
-            })
-
+            WeatherService(context).updateWeatherWidget(
+                it.latitude.toString(),
+                it.longitude.toString(),
+                remoteViews = views,
+                appWidgetId = appWidgetId,
+                appWidgetManager = appWidgetManager
+            )
         }
     }
 }
--- app/src/main/java/com/a2krocks/widget/services/WeatherService.kt
+++ app/src/main/java/com/a2krocks/widget/services/WeatherService.kt
@@ -1,31 +1,125 @@
 package com.a2krocks.widget.services
 
 import android.annotation.SuppressLint
+import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.util.Log
+import android.widget.RemoteViews
+import android.widget.Toast
 import com.a2krocks.widget.R
 import com.a2krocks.widget.services.data.WeatherData
+import com.google.gson.Gson
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.*
+import kotlin.math.roundToInt
 
-class WeatherService(val context: Context) {
+class WeatherService(private val context: Context) {
 
 
     private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private val pref = context.getSharedPreferences("WeatherData", Context.MODE_PRIVATE)
+    private val retrofitBuilder = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+        .create(WeatherInterface::class.java)
 
 
-    fun getWeather(lat: String, lon: String): Call<WeatherData> {
-        val retrofitBuilder = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-            .create(WeatherInterface::class.java)
-        return retrofitBuilder.getData(
+    /**
+     *  It updates Widgets with Weather Data.
+     *
+     *  @param lat Latitude
+     *  @param lon Longitude
+     *  @param remoteViews RemoteViews
+     *  @param appWidgetId App Widget Id
+     *  @param appWidgetManager App Widget Manager
+     *
+     *  @author Apurv Ajay Kumar ( aakapurv@gmail.com)
+     *
+     */
+    fun updateWeatherWidget(
+        lat: String,
+        lon: String,
+        remoteViews: RemoteViews,
+        appWidgetId: Int,
+        appWidgetManager: AppWidgetManager
+    ) {
+        val data = retrofitBuilder.getData(
             lat, lon, context.getString(R.string.apiId)
         )
+        data.enqueue(object : Callback<WeatherData> {
+            override fun onResponse(call: Call<WeatherData>, response: Response<WeatherData>) {
+                if (response.isSuccessful) {
+                    val weatherData = response.body()
+                    if (weatherData != null) {
+                        Toast.makeText(context, "Updated Weather Data", Toast.LENGTH_LONG).show()
+                        updateWeatherView(remoteViews = remoteViews, weatherData = weatherData)
+                    } else {
+                        Toast.makeText(context, "Weather Data is null", Toast.LENGTH_LONG).show()
+                        updateWeatherFromPreviousData(remoteViews = remoteViews)
+                    }
+                } else {
+                    val errorBody = response.errorBody()
+                    Toast.makeText(context, "Failed to load Weather Data", Toast.LENGTH_LONG).show()
+                    Log.d("response.isUnsuccessful", errorBody.toString())
+                    updateWeatherFromPreviousData(remoteViews = remoteViews)
+                }
+                appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+            }
+
+            override fun onFailure(call: Call<WeatherData>, t: Throwable) {
+                Log.e("response.failure", t.stackTraceToString())
+                Toast.makeText(context, "Failed to load Weather Data with throwable", Toast.LENGTH_LONG).show()
+                updateWeatherFromPreviousData(remoteViews = remoteViews)
+                appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+            }
+
+        })
+    }
+
+    private fun updateWeatherFromPreviousData(remoteViews: RemoteViews) {
+        val weatherDataJSON = pref.getString("WeatherData", null)
+        weatherDataJSON.let {
+            val weatherData = Gson().fromJson(weatherDataJSON, WeatherData::class.java)
+            updateWeatherView(remoteViews = remoteViews, weatherData = weatherData)
+        }
+    }
+
+    private fun setWeatherPref(weatherData: WeatherData) {
+        val edit = pref.edit()
+        edit.putString("WeatherData", Gson().toJson(weatherData))
+        edit.apply()
+    }
+
+    private fun updateWeatherView(remoteViews: RemoteViews, weatherData: WeatherData) {
+        setWeatherPref(weatherData = weatherData)
+        val temperature = weatherData.main.temp
+        val roundedTemperature = temperature.roundToInt()
+
+
+        val temperatureFormatted = "${roundedTemperature}°"
+        val weatherDescription = weatherData.weather[0].description.capitalizeWords()
+
+        val spannable = SpannableString(temperatureFormatted)
+        spannable.setSpan(
+            ForegroundColorSpan(Color.WHITE),
+            1,
+            temperatureFormatted.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        remoteViews.setTextViewText(R.id.tempTextView, spannable)
+        remoteViews.setTextViewText(R.id.descTextView, weatherDescription)
     }
 
 }
@@ -41,7 +135,7 @@ interface WeatherInterface {
 
 @SuppressLint("DefaultLocale")
 fun String.capitalizeWords(): String =
-    split(" ").joinToString(" ") {
-        it.lowercase(Locale.getDefault())
+    split(" ").joinToString(" ") { s ->
+        s.lowercase(Locale.getDefault())
             .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
     }
